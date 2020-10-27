50f1f78c746abf03370f8d0e5619fb0f2c977b37
gusta-el <gcabrerac@live.com>
2020-10-27 16:14:36 -0300

    CUTSCENE INICIAL

---
+++ Assets/Scripts/cutscene/GarrasEPenas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarrasEPenas : MonoBehaviour
+{
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = rectTransform.localScale;
+        Quaternion rotation = rectTransform.rotation;
+
+
+        pos.x = pos.x * 0.9995f;
+        pos.y = pos.y * 0.9995f;
+        
+
+        if(pos.y <= 0.35 && rotation.x <= 0.58)
+        {
+            rotation.x = rotation.x + 0.0008f;
+            rectTransform.rotation = rotation;
+        }
+
+        rectTransform.localScale = pos;
+
+    }
+}
