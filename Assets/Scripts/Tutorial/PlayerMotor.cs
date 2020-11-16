6add33697a5f24d7f9041a3f83a6c0288329648b
gusta-el <gcabrerac@live.com>
2020-11-15 22:59:16 -0300

    merge Erich -> Gustavo

--- Assets/Scripts/Tutorial/PlayerMotor.cs
+++ Assets/Scripts/Tutorial/PlayerMotor.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 [System.Serializable]
 public class Speed
 {
+    public float dead;
     public float run;
     public float walk;
 }
 
 public class PlayerMotor : MonoBehaviour
 {
+    private bool dead;
     public float _speed;
-    public Speed speed;
     private int _lookAt = 2;
+    public Speed speed;
 
     private Animator animator;
     private PolygonCollider2D polygon;
@@ -22,30 +24,52 @@ public class PlayerMotor : MonoBehaviour
     private SpriteRenderer playerSprite;
     private Transform arvoreTransform;
     private string TAG_ARVORE = "arvore";
-    
+
+    public Transform riverTransform;
+    private string TAG_river = "river";
+
+    private float spawn1_x;
+    private float spawn1_y;
+    private float spawn2_x;
+    private float spawn2_y;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerSprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         polygon = GetComponent<PolygonCollider2D>();
         playerSprite = GetComponent<SpriteRenderer>();
         arvoreTransform = GameObject.FindGameObjectWithTag(TAG_ARVORE).transform;
+        riverTransform = GameObject.FindGameObjectWithTag(TAG_river).transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-        colision();
     }
-    void Move()
+
+    void LateUpdate()
+    {
+        spawn1_x = riverTransform.position.x;
+        spawn1_x = riverTransform.position.y;
+        spawn2_x = riverTransform.position.x;
+        spawn2_x = riverTransform.position.y;
+
+        Vector3 riverPosition = riverTransform.position;
+    }
+        void Move()
     {
         //pega os inputs de horizontal e vertical predefinidos pela unity retornando um valor positivo ou negativo
         float _movX = Input.GetAxisRaw("Horizontal");
         float _movY = Input.GetAxisRaw("Vertical");
 
         //altera a velocidade do jogador caso ele esteja precionando o shift esquerdo
-        _speed = Input.GetKey(KeyCode.LeftShift) ? speed.run : speed.walk;
+        if (dead == false)
+        {
+            _speed = Input.GetKey(KeyCode.LeftShift) ? speed.run : speed.walk;
+        }
 
         Vector3 velocity = new Vector3(_movX, _movY, 0) * Time.deltaTime * _speed;
 
@@ -64,28 +88,18 @@ public class PlayerMotor : MonoBehaviour
         else if (velocity.x > 0 && velocity.y == 0) _lookAt = 3;
         else if (velocity.x > 0 && velocity.y > 0) _lookAt = 35;
 
-        if (transform.position.y < arvoreTransform.position.y)
-        {
-            playerSprite.sortingOrder = 5;
-        }
-        else
-        {
-            playerSprite.sortingOrder = 0;
-        }
-        
         animator.SetInteger("lookAt", _lookAt);
 
         transform.position += velocity;
     }
-    void colision()
+
+    void OnTriggerEnter2D(Collider2D col)
     {
-        if (_speed > speed.walk)
+        if (col.CompareTag("river"))
         {
-            polygon.enabled = false;
-        }
-        else
-        {
-            polygon.enabled = true;
-        }
+            playerSprite.enabled = false;
+            _speed = speed.walk;
+            dead = true;
+        }    
     }
 }
